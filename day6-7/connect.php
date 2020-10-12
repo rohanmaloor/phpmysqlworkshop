4a8dff4d832772719c383db0565de6ee64e4d376
Rohan Maloor <rohanmaloor@gmail.com>
2020-10-12 18:06:51 +0530

    Day6-7 commit

---
+++ day6-7/connect.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "", "result");
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected to database";
+
+/*
+LOGIN CREDENTIALS
+ADMIN:
+admin-Adminpass
+USERS:
+rorobro-Password1
+jacklow-Qwerty
+harleyq-Joker1
+rondah-Qazwsx
+amys-ilyjake
+heisenburg-saymyname
+*/
+
+
+
+?>
